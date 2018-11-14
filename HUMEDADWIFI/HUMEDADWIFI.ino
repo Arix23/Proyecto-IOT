85689fe0b5d0fe792dcabc97fc08631d3cfd163f
Arix23 <arivalenzuela23@gmail.com>
2018-11-13 20:19:45 -0600

    Update HUMEDADWIFI/HUMEDADWIFI.ino

---
+++ HUMEDADWIFI/HUMEDADWIFI.ino
@@ -0,0 +1,69 @@
+int digitalPin = 16; // sensor Humedad
+int analogPin= 0; //Servo
+const int buzzer = 4; 
+int LED=5; //Pin del Led
+int sensorValue = analogRead(A0);   // read the input on analog pin 0
+#include <Servo.h> // including servo library-
+Servo servo_1; // Giving name to servo.
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "bef93b8c05e34908947a688243fce19a";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Android AP";
+char pass[] = "quimica1234";
+
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600); 
+
+  Blynk.begin(auth, ssid, pass);
+  pinMode(digitalPin, INPUT);
+  servo_1.attach(0); // Attaching Servo to D3
+  servo_1.write(0);
+  pinMode(buzzer,OUTPUT);
+  pinMode(LED,OUTPUT);
+}
+
+void loop()
+{
+  Blynk.run();
+  
+  /*
+   * High == Dry0
+   * Low == Wet
+   */
+  if (digitalRead(digitalPin)>0.75){
+    Serial.println("Dry");
+    servo_1.write(360); // Servo will move to 45 degree angle.
+    delay (1000);
+    servo_1.write(0); // Servo will move to 45 degree angle.
+    delay (1000);
+    digitalWrite(buzzer, HIGH);
+    delay(1000);
+    digitalWrite(buzzer, LOW);
+    delay(1000);
+    Serial.println("Beep");
+    digitalWrite(LED,HIGH);      
+  
+  }
+  else if(digitalRead(digitalPin)<=0.75){    
+    Serial.println("Wet");
+     
+  }
+  else{
+    Serial.println("Error");
+  }
+
+  Serial.println(sensorValue); 
+
+  delay(1000);  
+}
